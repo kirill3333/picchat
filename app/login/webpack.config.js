61c603bb098de31cd041b9c3f4cb4c5d15857ad8
kirill3333 <kirill3333@inbox.ru>
2017-09-22 00:41:28 +0300

    antd config

--- app/login/webpack.config.js
+++ app/login/webpack.config.js
@@ -1,14 +1,24 @@
-const path = require('path');
+const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
 module.exports = {
-  entry: './client/index.js',
+  entry: './src/app.js',
   output: {
     path: path.resolve('dist'),
-    filename: 'index_bundle.js'
+    filename: 'app.js'
   },
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
     ]
-  }
+  },
+  plugins: [HtmlWebpackPluginConfig]
 }
