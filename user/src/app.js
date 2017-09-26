a8d019cf8080acc669eb0e11228045115dcfdae5
kirill3333 <kirill3333@inbox.ru>
2017-09-26 22:43:45 +0300

    app setup

--- user/src/app.js
+++ user/src/app.js
@@ -1,10 +1,17 @@
+require('dotenv').config()
 const express = require('express')
+const winston = require('winston')
+const bodyParser = require('body-parser')
+const registerRouter = require('./router/register')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+winston.level = 'debug'
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/user', registerRouter)
 
 app.listen(6001, function () {
-  console.log('user service started at 6001')
+  winston.log('info', 'User service started at 6001')
 })
