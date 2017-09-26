3fb2ea7f609a45eaa2a6493c8c20a064c139fd40
kirill3333 <kirill3333@inbox.ru>
2017-09-26 22:43:32 +0300

    implement register functionality

--- user/src/router/register.js
+++ user/src/router/register.js
@@ -1,9 +1,32 @@
 const express = require('express')
 const router = express.Router()
+const winston = require('winston')
 const userService = require('../service/user')
 
+function validateUser(user) {
+  return Promise.all([
+    userService.checkByName(user.nick),
+    userService.checkByPhone(user.mobile)
+  ])
+    .then((results) => {
+      if (results[0] && results[1]) return user
+      throw new Error('Usr is not valid')
+    })
+}
+
 router.post('/register', (req, res) => {
-  Promise.all([userService.checkByName(), userService.checkByPhone()])
-    .then()
-    .catch()
+  const user = {
+    password: req.body.password,
+    mobile: req.body.mobile,
+    nick: req.body.nick
+  }
+  validateUser(user)
+    .then(() => userService.create(user))
+    .then(() => res.status(200).send('User created'))
+    .catch((error) => {
+      winston.log('debug', error)
+      res.status(500).send('Error to create user')
+    })
 })
+
+module.exports = router
