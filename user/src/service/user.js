9d43148af7700d75ed6b9460a3f22879c7ea26fa
kirill3333 <kirill3333@inbox.ru>
2017-09-26 22:43:13 +0300

    implement user service

--- user/src/service/user.js
+++ user/src/service/user.js
@@ -1,17 +1,62 @@
+const squel = require('squel')
+const uuidv4 = require('uuid/v4')
+const passwordHash = require('password-hash')
+const connector = require('../db/mysql.connector')
+
 function checkByName(name) {
-  return Promise.resolve(true)
+  if (!name) return Promise.reject('Empty name')
+
+  const query = squel.select()
+    .from("user")
+    .where('nick = ?', name)
+    .toString()
+  return connector.query(query)
+    .then((results) => {
+      if (results && results.length > 0) return false
+      return true
+    })
+    .catch((error) => {
+      winston.log('debug', error)
+      return false
+    })
 }
 
-function checkByPhone(phone) {
-  return Promise.resolve(true)
+function checkByPhone(mobile) {
+  if (!mobile) return Promise.reject('Empty mobile')
+
+  const query = squel.select()
+    .from("user")
+    .where('mobile = ?', mobile)
+    .toString()
+  return connector.query(query)
+    .then((results) => {
+      if (results && results.length > 0) return false
+      return true
+    })
+    .catch((error) => {
+      winston.log('debug', error)
+      return false
+    })
 }
 
-function saveUser(user) {
-  return Promise.resolve(true)
+function createUser(user) {
+  const uuid = uuidv4()
+  const hashedPassword = passwordHash.generate(user.password)
+
+  const query = squel
+    .insert()
+    .into('user')
+    .set('uuid', uuid)
+    .set('password', hashedPassword)
+    .set('mobile', user.mobile)
+    .set('nick', user.nick)
+    .toString()
+
+  return connector.process(query)
 }
 
 module.exports = {
   checkByName: checkByName,
   checkByPhone: checkByPhone,
-  saveUser: saveUser
+  create: createUser
 }
